d6beb2ac0ce8caae595f61759bc9a38f5b43d73d
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-04-24 18:32:54 +0900

    class MainActivity : AppCompatActivity(), View.OnClickListener{

--- app/src/main/java/com/example/ui/MainActivity.kt
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1.text = "ぼたん"
+        button1.setOnClickListener(this)
+    }
+    override fun onClick(v: View){
+        Log.d("UI_PARTS", "ボタンをタップしました")
     }
 }
