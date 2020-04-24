bdaddb6f2e9ba7e5d45a89be71ee78fedbe01cc0
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-04-24 18:42:45 +0900

    Log.d("UI_PARTS", "ボタンをタップしました")

--- app/src/main/java/com/example/ui/MainActivity.kt
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -3,18 +3,16 @@ package com.example.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener{
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1.setOnClickListener(this)
-    }
-    override fun onClick(v: View){
+        button1.setOnClickListener {
         Log.d("UI_PARTS", "ボタンをタップしました")
     }
+}
 }
