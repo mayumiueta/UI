e4aab49840ec1385ad2358f52f037f3b1bc26baf
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-04-25 19:14:05 +0900

    TimePickerDialog.OnTimeSetListener{ view, hour, minute ->

--- app/src/main/java/com/example/ui/MainActivity.kt
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ui
 
+import android. app.TimePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
@@ -15,39 +16,53 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
 
         button1.setOnClickListener (this)
         button2.setOnClickListener (this)
+        button3.setOnClickListener (this)
     }
-      override fun onClick(v: View)  {
-          if(v.id == R.id.button1) {
-     textView.text = editText.text.toString()
-    }else if (v.id == R.id.button2){
-              showAlertDialog()
-          }
-}
+    override fun onClick(v: View)  {
+        if(v.id == R.id.button1) {
+            textView.text = editText.text.toString()
+        }else if (v.id == R.id.button2){
+            showAlertDialog()
+        }else if (v.id == R.id.button3){
+            showTimePickerDialog()
+        }
+    }
 
-private fun showAlertDialog(){
+    private fun showAlertDialog(){
 //AlertDialog.Builderクラスを使ってAlertDialogの準備をする
-    val alertDialogBuilder = AlertDialog.Builder(this)
-    alertDialogBuilder.setTitle("タイトル")
-    alertDialogBuilder.setMessage("メッセージ")
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("タイトル")
+        alertDialogBuilder.setMessage("メッセージ")
 
-    //肯定ボタンに表示される文字列、押したときのリスナーを設定する
-    alertDialogBuilder.setPositiveButton("肯定"){dialog, which ->
-        Log.d("UI_PARTS", "肯定ボタン")
+        //肯定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setPositiveButton("肯定"){dialog, which ->
+            Log.d("UI_PARTS", "肯定ボタン")
+        }
+
+        //中立ボタンに表示される文字列、押したときのリスナーを設定する
+        //使わない引数の場合は「＿」と記述するのがkotlinの慣習
+        alertDialogBuilder.setNeutralButton("中立"){_,_->
+            Log.d("UI_PARTS", "中立ボタン")
+        }
+
+        //否定ボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setNegativeButton("否定"){_,_ ->
+            Log.d("UI_PARTS", "否定ボタン")
+        }
+
+        // AlertDialogを作成して表示する
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 
-    //中立ボタンに表示される文字列、押したときのリスナーを設定する
-    //使わない引数の場合は「＿」と記述するのがkotlinの慣習
-    alertDialogBuilder.setNeutralButton("中立"){_,_->
-        Log.d("UI_PARTS", "中立ボタン")
+    private fun showTimePickerDialog(){
+        val timePickerDialog = TimePickerDialog(
+            this
+                    TimePickerDialog.OnTimeSetListener{ view, hour, minute ->
+                Log.d("UI_PARTS", "$hour:$minute")
+            },
+            13, 0, true)
+        timePickerDialog.show()
     }
-
-    //否定ボタンに表示される文字列、押したときのリスナーを設定する
-    alertDialogBuilder.setNegativeButton("否定"){_,_ ->
-        Log.d("UI_PARTS", "否定ボタン")
-    }
-
-    // AlertDialogを作成して表示する
-val alertDialog = alertDialogBuilder.create()
-    alertDialog.show()
 }
-}
+
